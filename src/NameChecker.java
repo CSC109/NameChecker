d757bcdfe1801f3bd84aa828cdae7fbef2c7d216
a-r-t <alexthimineur@gmail.com>
2021-02-16 16:51:41 -0500

    initial commit

---
+++ src/NameChecker.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class NameChecker {
+
+	public static void main(String[] args) {
+		Scanner keyboardInput = new Scanner(System.in);
+		System.out.println("What is your first name?");
+		String name = keyboardInput.nextLine();
+		if (name.equals("Alex")) {
+			System.out.println("Nice name! Wow! Love it!");
+		}
+		else {
+			System.out.println("That's an okay name I guess.");
+		}
+	}
+
+}
